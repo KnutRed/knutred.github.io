9bd41b5098289816abde937e94e1d76a5fa7e58d
Knut Red <38220896+KnutRed@users.noreply.github.com>
2024-09-17 19:36:59 +0200

    Update site URL to use HTTP instead of HTTPS

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   
-  site: 'https://knut.roteelster.de',
+  site: 'http://knut.roteelster.de',
   
   output: 'static',
   integrations: [tailwind()],
