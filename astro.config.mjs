6ca844e999dcbe7c9281391b0d1c3cfe2a87239f
Knut Red <38220896+KnutRed@users.noreply.github.com>
2025-11-11 19:17:07 +0100

    chore: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
+
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   site: "https://knut.roteelster.de",
-
   output: "static",
-  integrations: [tailwind()],
+  integrations: [],
+
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
